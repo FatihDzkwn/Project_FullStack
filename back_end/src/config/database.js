2fd492fafc46020257d01adec53f0ae54c02930b
Fatih Dzakwan Susilo <fatihdzakwansusilo@gmail.com>
2026-04-17 15:27:11 +0700

    Commit

--- back_end/src/config/database.js
+++ back_end/src/config/database.js
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'sky_booking',
   waitForConnections: true,
   connectionLimit: 10,
