d1afa8e2c52e2e149c2dcc61b639e3444d35a04b
Fatih Dzakwan Susilo <fatihdzakwansusilo@gmail.com>
2026-06-25 10:58:24 +0700

    Merubah Koneksi Database

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -1,35 +1,28 @@
-const mysql = require('mysql2'); // Pastikan sudah install mysql2
+const mysql = require('mysql2');
 
 const config = {
   APP_NAME: process.env.APP_NAME || 'SkyBooking',
   PORT: process.env.PORT || 5000,
-  
-  // TAMBAHKAN BAGIAN INI
   DB: {
     HOST: process.env.DB_HOST || 'localhost',
     USER: process.env.DB_USER || 'root',
     PASSWORD: process.env.DB_PASSWORD || '',
-    NAME: process.env.DB_NAME || 'flight_booking_db',
+    NAME: process.env.DB_NAME || 'flight_booking_db'
   },
-
   AUTH: {
     SECRET: process.env.JWT_SECRET,
-    EXPIRY: process.env.JWT_EXPIRY || '7d',
-  },
-  
-
+    EXPIRY: process.env.JWT_EXPIRY || '7d'
+  }
 };
 
 const db = mysql.createPool({
-  uri: process.env.DATABASE_URL || 'mysql://root:AARCsfXzm0IwbjcVMohTUqEYttSYJD@mysql.railway.internal:3306/railway',
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  // Kunci utama agar diizinkan masuk ke database cloud luar:
   ssl: {
     rejectUnauthorized: false
   }
 });
-
 
 module.exports = { config, db };
