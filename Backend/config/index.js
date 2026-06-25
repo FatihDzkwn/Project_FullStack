a2c8432288198be3320ca67eab0e741ff633704e
Fatih Dzakwan Susilo <fatihdzakwansusilo@gmail.com>
2026-06-25 08:14:48 +0700

    Merubah Koneksi Database

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -21,12 +21,14 @@ const config = {
 
 };
 
+const mysql = require('mysql2');
+
 const db = mysql.createPool({
-  host: 'mysql.railway.internal',
-  user: 'root',
-  password: 'AARCsfXzm0IwbjcVMohTUqEYttSYJD',
-  database: 'railway',
-  port: 3306,
+  host: process.env.MYSQLHOST || 'mysql.railway.internal',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'AARCsfXzm0IwbjcVMohTUqEYttSYJD',
+  database: process.env.MYSQLDATABASE || 'railway',
+  port: parseInt(process.env.MYSQLPORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
